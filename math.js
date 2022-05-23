dfa122ba7535c30294b83b6c79b4bdefaf094b27
Nimra <nimrarehman228@gmail.com>
2022-05-23 21:16:15 +0500

    Javascript assignment 2

---
+++ math.js
@@ -0,0 +1,72 @@
+// document.write("Result:");
+// document.write("<br>");
+
+// var a = 10;
+// document.write("The value of a is: " + a);
+// document.write("<br>");
+
+// document.write("...................................");
+// document.write("<br>");
+// document.write("<br>");
+
+// var b = ++a;
+// document.write("The value of ++a is: " + b);
+// document.write("<br>");
+// document.write("Now the value of a is: " + b);
+// document.write("<br>");
+// document.write("<br>");
+
+// var a = 11;
+// document.write("The value of a++ is: " + a);
+// document.write("<br>");
+// a++;
+// document.write("Now the value of a is: " + a);
+// document.write("<br>");
+// document.write("<br>");
+
+// var a = 12;
+// --a;
+// document.write("The value of --a is: " + a);
+// document.write("<br>");
+// document.write("Now the value of a is: " + a);
+// document.write("<br>");
+// document.write("<br>");
+// document.write("The value of a-- is: " + a);
+// document.write("<br>");
+// var a = 11;
+// a--;
+// document.write("Now the value of a is: " + a);
+// document.write("<br>");
+// document.write("<br>");
+
+
+var a = 2;
+var b = 1;
+
+document.write("a is " + a);
+document.write("<br>");
+document.write("b is " + b);
+document.write("<br>");
+document.write("<br>");
+
+var addA = --a;
+document.write("--a is " + addA);
+document.write("<br>");
+document.write("<br>");
+
+var Ab3 = --a - --b + ++b;
+document.write("--a - --b + ++b is " + Ab3);
+document.write("<br>");
+document.write("<br>");
+
+
+var addAb = --a - --b;
+document.write("--a - --b is " + addAb);
+document.write("<br>");
+document.write("<br>");
+
+var result = --a - --b + ++b + b--;
+document.write("result is " + result);
+document.write("<br>");
+document.write("<br>");
+
